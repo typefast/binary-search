8b742ce12d4c70af75a07befb2bbefc4806d5401
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-10 21:05:34 +0000

    Add binarySearch method

---
+++ README.md
@@ -0,0 +1,1 @@
+Binary search of an array
---
+++ binarysearch.rb
@@ -0,0 +1,21 @@
+
+def binarySearch(array, target)
+  min = 0
+  max = array.length - 1
+  
+  loop do
+    guess = ((min + max) / 2).floor
+    return "Nope not there" if max < min 
+    if array[guess] == target
+      return guess
+    elsif array[guess] < target 
+      min = guess + 1
+    else
+      max = guess - 1
+    end
+  end
+end
+
+values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+puts binarySearch(values, 17)
+
