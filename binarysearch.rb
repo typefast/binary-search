aac95eee38e4992868e70e2b5ad6e25268067c3a
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-10 21:11:42 +0000

    Add print message

--- binarysearch.rb
+++ binarysearch.rb
@@ -17,5 +17,5 @@ def binarySearch(array, target)
 end
 
 values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-puts binarySearch(values, 17)
-
+index_result =  binarySearch(values, 17)
+puts "Found prime at index: #{index_result}"
